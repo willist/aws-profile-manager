2232dfe23e35e764687ba0d0506761f8c22c1351
Tim Willis <tim@willishq.com>
2022-06-03 12:00:30 -0500

    Initial commit

---
+++ configure_profiles.py
@@ -0,0 +1,68 @@
+import configparser
+from pathlib import Path
+
+import boto3
+
+from sso_login import sso_login
+
+
+client = boto3.client('sso')
+token = sso_login()
+
+# TODO: Move this to a config file
+sso_profile_prefix = "sso"
+role_mapping = {
+    'Admin': 'admin',
+    'Audit': 'audit',
+    'ReadOnly': 'read',
+}
+
+valid_sso_profiles = set()
+
+
+aws_config_file = Path.home().joinpath(".aws/config")
+
+# Read AWS Profile Config
+aws_config = configparser.ConfigParser()
+with aws_config_file.open() as f:
+    aws_config.read_file(f)
+
+# Backup Config file
+with aws_config_file.with_suffix('.bak').open('w') as wf:
+    aws_config.write(wf)
+
+# Iterate through available accounts and roles; add and update profiles
+account_paginator = client.get_paginator('list_accounts')
+role_paginator = client.get_paginator('list_account_roles')
+for account in account_paginator.paginate(accessToken=token).search('accountList'):
+    name = account['accountName'].replace(' ', '-').lower()
+    account_id = account['accountId']
+
+    for role in role_paginator.paginate(accessToken=token, accountId=account_id).search('roleList'):
+        role_name = role_mapping.get(role['roleName'], role['roleName'])
+        profile_name = "profile " + "-".join(filter(None, [sso_profile_prefix, name, role_name]))
+
+        valid_sso_profiles.add(profile_name)
+
+        try:
+            profile_config = dict(aws_config[profile_name])
+        except KeyError:
+            profile_config = {}
+
+        profile_config['sso_start_url'] = 'https://divvydose.awsapps.com/start'
+        profile_config['sso_account_id'] = role['accountId']
+        profile_config['sso_role_name'] = role['roleName']
+        profile_config['sso_region'] = 'us-west-2'
+        profile_config['region'] = 'us-west-2'
+
+        aws_config[profile_name] = profile_config
+
+# Remove outdated sso- profiles
+all_sso_profiles = set(s for s in aws_config.sections() if s.startswith('profile sso'))
+invalid = all_sso_profiles - valid_sso_profiles
+for profile in invalid:
+    del aws_config[profile]
+
+# Write new version of aws profile config
+with aws_config_file.open('w') as wf:
+    aws_config.write(wf)
---
+++ sso_login.py
@@ -0,0 +1,56 @@
+from datetime import datetime, timedelta
+import json
+import time
+import webbrowser
+from pathlib import Path
+
+import boto3
+
+cache = Path.home().joinpath(".aws/sso/cache")
+cache.mkdir(exist_ok=True, parents=True)
+
+oidc = boto3.client('sso-oidc')
+
+
+def sso_login(start_url='https://divvydose.awsapps.com/start#/'):
+    cached_token_path = cache.joinpath('profile_manager_token.json')
+    try:
+        with cached_token_path.open() as f:
+            cached_token = json.load(f)
+            if cached_token['expires'] > datetime.now().timestamp():
+                return cached_token['token']
+    except FileNotFoundError:
+        pass
+
+    client_response = oidc.register_client(
+        clientName='profile_manager',
+        clientType='public',
+    )
+
+    auth_response = oidc.start_device_authorization(
+        clientId=client_response['clientId'],
+        clientSecret=client_response['clientSecret'],
+        startUrl=start_url,
+    )
+
+    webbrowser.open(auth_response["verificationUriComplete"])
+
+    while True:
+        try:
+            token_response = oidc.create_token(
+                clientId=client_response['clientId'],
+                clientSecret=client_response['clientSecret'],
+                grantType='urn:ietf:params:oauth:grant-type:device_code',
+                deviceCode=auth_response['deviceCode'],
+            )
+            expires = datetime.now() + timedelta(seconds=token_response['expiresIn'])
+            with cached_token_path.open('w') as f:
+                to_cache = {
+                    'expires': expires.timestamp(),
+                    'token': token_response['accessToken']
+                }
+                json.dump(to_cache, f)
+
+            return token_response['accessToken']
+        except oidc.exceptions.AuthorizationPendingException:
+            time.sleep(1)
